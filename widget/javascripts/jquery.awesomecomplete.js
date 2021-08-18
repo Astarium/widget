03e172e8576ce3274741157b00c042d9ac87ab02
Maciej Kacprzak <paramaciej@gmail.com>
2021-08-18 12:59:31 +0200

    reformat jquery.awesomecomplete.js

--- widget/javascripts/jquery.awesomecomplete.js
+++ widget/javascripts/jquery.awesomecomplete.js
@@ -5,18 +5,15 @@
  *    wild under public domain, but please do let me know what you think!
  */
 
-(function($)
-{
+(function ($) {
     var ident = 0,
         scrollIntoView = ('scrollIntoView' in document.createElement('li'));
 
     // Initializer. Call on a text field to make things go.
-    $.fn.awesomecomplete = function(options)
-    {
+    $.fn.awesomecomplete = function (options) {
         var options = $.extend({}, $.fn.awesomecomplete.defaults, options);
 
-        return this.each(function()
-        {   
+        return this.each(function () {
             var $this = $(this);
             var config = $.meta ? $.extend({}, options, $this.data()) : options;
             $this.data('awesomecomplete-config', config);
@@ -24,47 +21,43 @@
             var $attachTo = $(config.attachTo || $this);
             $this.data('awesomecomplete-attachTo', $attachTo);
             var $list = $('<ul/>').addClass(config.suggestionListClass)
-                                  .insertAfter($attachTo)
-                                  .hide()
-                                  .css('width', $attachTo.outerWidth());
+                .insertAfter($attachTo)
+                .hide()
+                .css('width', $attachTo.outerWidth());
             $this.data('awesomecomplete-list', $list);
-            $(window).resize(function() { $list.css('width', $attachTo.outerWidth());});
-            
+            $(window).resize(function () {
+                $list.css('width', $attachTo.outerWidth());
+            });
+
             var typingDelayPointer;
             var suppressKey = false;
-            $this.keyup(function(event)
-            {
-                if (suppressKey)
-                {
+            $this.keyup(function (event) {
+                if (suppressKey) {
                     suppressKey = false;
                     return;
                 }
 
                 // ignore arrow keys, shift
-                if ( ((event.which > 36) && (event.which < 41)) ||
-                     (event.which == 16) )
+                if (((event.which > 36) && (event.which < 41)) ||
+                    (event.which == 16))
                     return;
 
-                if (config.typingDelay > 0)
-                {
+                if (config.typingDelay > 0) {
                     clearTimeout(typingDelayPointer);
-                    typingDelayPointer = setTimeout(function() { processInput($this); }, config.typingDelay);
-                }
-                else
-                {
+                    typingDelayPointer = setTimeout(function () {
+                        processInput($this);
+                    }, config.typingDelay);
+                } else {
                     processInput($this);
                 }
             });
 
-            $this.keydown(function(event)
-            {
+            $this.keydown(function (event) {
                 // tab = 9; enter = 13; up = 38; down = 40; esc = 27
                 var $active = $list.children('li.' + config.activeItemClass);
-                switch (event.which)
-                {
+                switch (event.which) {
                     case 9:
-                        if (($active.length !== 0) && ($list.is(':visible')))
-                        {
+                        if (($active.length !== 0) && ($list.is(':visible'))) {
                             $this.val($active.data('awesomecomplete-value'));
                             config.onComplete($active.data('awesomecomplete-dataItem'));
                             $list.hide();
@@ -74,8 +67,7 @@
                         suppressKey = true;
                         break;
                     case 13:
-                        if (($active.length !== 0) && ($list.is(':visible')))
-                        {
+                        if (($active.length !== 0) && ($list.is(':visible'))) {
                             event.preventDefault();
                             $this.val($active.data('awesomecomplete-value'));
                             config.onComplete($active.data('awesomecomplete-dataItem'));
@@ -87,24 +79,18 @@
                         break;
                     case 38:
                         event.preventDefault();
-                        if ($active.length === 0)
-                        {
+                        if ($active.length === 0) {
                             $list.children('li:last-child').addClass(config.activeItemClass);
-                        }
-                        else
-                        {
+                        } else {
                             $active.prev().addClass(config.activeItemClass);
                             $active.removeClass(config.activeItemClass);
                         }
                         break;
                     case 40:
                         event.preventDefault();
-                        if ($active.length === 0)
-                        {
+                        if ($active.length === 0) {
                             $list.children('li:first-child').addClass(config.activeItemClass);
-                        }
-                        else if ($active.is(':not(:last-child)'))
-                        {
+                        } else if ($active.is(':not(:last-child)')) {
                             $active.next().addClass(config.activeItemClass);
                             $active.removeClass(config.activeItemClass);
                         }
@@ -122,49 +108,39 @@
                 }
             });
 
-	    // opera wants keypress rather than keydown to prevent the form submit
-            $this.keypress(function(event)
-            {
+            // opera wants keypress rather than keydown to prevent the form submit
+            $this.keypress(function (event) {
                 var $active = $list.children('li.' + config.activeItemClass);
 
-		if ((event.which == 13) && ($list.children('li.' + config.activeItemClass).length > 0))
-		{
-		    event.preventDefault();
-		}
-	    });
+                if ((event.which == 13) && ($list.children('li.' + config.activeItemClass).length > 0)) {
+                    event.preventDefault();
+                }
+            });
 
             // stupid hack to get around loss of focus on mousedown
             var mouseDown = false;
             var blurWait = false;
-            $(document).bind('mousedown.awesomecomplete' + ++ident, function()
-            {
+            $(document).bind('mousedown.awesomecomplete' + ++ident, function () {
                 mouseDown = true;
             });
-            $(document).bind('mouseup.awesomecomplete' + ident, function()
-            {
+            $(document).bind('mouseup.awesomecomplete' + ident, function () {
                 mouseDown = false;
-                if (blurWait)
-                {
+                if (blurWait) {
                     blurWait = false;
                     $list.hide();
                 }
             });
-            $this.blur(function()
-            {
-                if (mouseDown)
-                {
+            $this.blur(function () {
+                if (mouseDown) {
                     blurWait = true;
-                }
-                else
-                {
+                } else {
                     var $active = $list.children('li.' + config.activeItemClass);
                     if ($list.is(':visible') && ($active.length !== 0))
                         $this.val($active.data('awesomecomplete-value'));
                     $list.hide();
                 }
             });
-            $this.focus(function()
-            {
+            $this.focus(function () {
                 if ($list.children(':not(.' + config.noResultsClass + ')').length > 0) {
                     $list.css('width', $attachTo.outerWidth());
                     $list.show();
@@ -176,17 +152,14 @@
     // Data callback.  If you're using callbacks to a server,
     // call this on the autocompleted text field to complete the
     // callback process after you have your matching items.
-    var onDataProxy = function($this, term)
-    {
-        return function(data)
-        {
+    var onDataProxy = function ($this, term) {
+        return function (data) {
             processData($this, data, term);
         };
     }
 
-// private helpers
-    var processInput = function($this)
-    {
+    // private helpers
+    var processInput = function ($this) {
         var term = $this.val();
         if (term.length > 1) {
             $this.parent().find('.arrow-down').show();
@@ -197,8 +170,7 @@
         }
     };
 
-    var processData = function($this, data, term)
-    {
+    var processData = function ($this, data, term) {
         var $list = $this.data('awesomecomplete-list');
         $list.empty().hide();
         if (term === '')
@@ -207,8 +179,7 @@
         var config = $this.data('awesomecomplete-config');
 
         var results = [];
-        for (var item = 0; item < data.length; item++)
-        {
+        for (var item = 0; item < data.length; item++) {
             var dataItem = jQuery.extend({}, data[item]);
             var matchCount = 0;
 
@@ -216,8 +187,7 @@
             var topMatch = null;
             var matchedTerms = [];
 
-            for (var field in dataItem)
-            {
+            for (var field in dataItem) {
                 if ((typeof dataItem[field] === 'function') || (typeof dataItem[field] === 'object'))
                     continue;
 
@@ -229,12 +199,11 @@
                     continue;
 
                 var dataString = dataItem[field].toString();
-                var terms = [ term ];
+                var terms = [term];
                 if (config.splitTerm)
                     terms = term.split(config.wordDelimiter);
 
-                for (var j = 0; j < terms.length; j++)
-                {
+                for (var j = 0; j < terms.length; j++) {
                     if (terms[j] === '')
                         continue;
 
@@ -242,12 +211,10 @@
                     var regex = new RegExp('(^| )(' + terms[j] + ')', 'i');
 
                     var matches = [];
-                    if (matches = dataString.match(regex))
-                    {
+                    if (matches = dataString.match(regex)) {
                         matchCount += matches.length;
 
-                        if ((field != config.nameField) && (matches.length > maxFieldMatches))
-                        {
+                        if ((field != config.nameField) && (matches.length > maxFieldMatches)) {
                             maxFieldMatches = matches.length;
                             topMatch = field;
                             matchedTerms[j] = true;
@@ -255,8 +222,7 @@
                     }
                 }
 
-                if (config.highlightMatches)
-                {
+                if (config.highlightMatches) {
                     var regex = new RegExp('(' + terms.join('|') + ')', (config.ignoreCase ? 'ig' : 'g'));
                     dataItem[field] = dataString.replace(regex, '<span class="' + config.highlightClass + '">$1</span>');
                 }
@@ -277,29 +243,26 @@
                 });
         }
 
-        results.sort(function(a, b)
-        {
+        results.sort(function (a, b) {
             return config.sortFunction(a, b, term);
         });
 
         results = results.slice(0, config.resultLimit);
 
-        results.forEach(function(item, i, enumerable) {
+        results.forEach(function (item, i, enumerable) {
             $('<li>' + config.renderFunction(results[i].dataItem, results[i].topMatch, results[i].originalDataItem, config) + '</li>')
-				.data('awesomecomplete-dataItem', results[i].originalDataItem)
+                .data('awesomecomplete-dataItem', results[i].originalDataItem)
                 .data('awesomecomplete-value', config.valueFunction(results[i].originalDataItem, config))
                 .appendTo($list)
-                .click(function()
-                {
+                .click(function () {
                     var $listItem = $(this);
                     $this.val($listItem.data('awesomecomplete-value'));
                     config.onComplete($listItem.data('awesomecomplete-dataItem'));
                     $this.change();
                 })
-                .mouseover(function()
-                {
+                .mouseover(function () {
                     $(this).addClass(config.activeItemClass)
-                           .siblings().removeClass(config.activeItemClass);
+                        .siblings().removeClass(config.activeItemClass);
                 });
         });
 
@@ -312,27 +275,24 @@
         }
     };
 
-// default functions
-    var defaultRenderFunction = function(dataItem, topMatch, originalDataItem, config)
-    {
+    // default functions
+    var defaultRenderFunction = function (dataItem, topMatch, originalDataItem, config) {
         if ((topMatch === config.nameField) || (topMatch === null))
             return '<p class="title">' + dataItem[config.nameField] + '</p>';
         else
             return '<p class="title">' + dataItem[config.nameField] + '</p>' +
-                   '<p class="matchRow"><span class="matchedField">' + topMatch + '</span>: ' +
-                        dataItem[topMatch] + '</p>';
+                '<p class="matchRow"><span class="matchedField">' + topMatch + '</span>: ' +
+                dataItem[topMatch] + '</p>';
     };
-    
-    var defaultValueFunction = function(dataItem, config)
-    {
+
+    var defaultValueFunction = function (dataItem, config) {
         return dataItem[config.nameField];
     };
 
-    var defaultSortFunction = function(a, b, term)
-    {
+    var defaultSortFunction = function (a, b, term) {
         return (a.matchedTermCount == b.matchedTermCount) ?
-               (b.matchCount - a.matchCount) :
-               (b.matchedTermCount - a.matchedTermCount);
+            (b.matchCount - a.matchCount) :
+            (b.matchedTermCount - a.matchedTermCount);
     };
 
     $.fn.awesomecomplete.defaults = {
@@ -346,7 +306,8 @@
         nameField: 'name',
         noResultsClass: 'noResults',
         noResultsMessage: undefined,
-        onComplete: function(dataItem) {},
+        onComplete: function (dataItem) {
+        },
         sortFunction: defaultSortFunction,
         splitTerm: true,
         staticData: [],
